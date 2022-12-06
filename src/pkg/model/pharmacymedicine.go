195ab6449cca32a4dad4830fb21840db4348e132
Indra <indra@ide.web.id>
2022-12-06 21:40:45 +0700

    add length pharmacy

--- src/pkg/model/pharmacymedicine.go
+++ src/pkg/model/pharmacymedicine.go
@@ -5,9 +5,9 @@ type PharmacyMedicine struct {
 	Pharmacy    Pharmacy
 	MedicineID  uint64 `json:"medicine_id"`
 	Medicine    Medicine
-	Name        string `gorm:"size:55" json:"name"`
-	Quantity    uint   `gorm:"size:8" json:"quantity"`
-	Rule        string `gorm:"size:55" json:"rule"`
-	Dose        string `gorm:"size:15" json:"dose"`
+	Name        string `gorm:"size:250" json:"name"`
+	Quantity    uint   `gorm:"size:9" json:"quantity"`
+	Rule        string `gorm:"size:250" json:"rule"`
+	Dose        string `gorm:"size:250" json:"dose"`
 	Description string `gorm:"type:text" json:"description"`
 }
