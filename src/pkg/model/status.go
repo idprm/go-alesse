c7f6d75712866f7444a5cfe63caa0d8c9f74da85
Indra <indra@ide.web.id>
2022-12-06 23:11:21 +0700

    add new status

--- src/pkg/model/status.go
+++ src/pkg/model/status.go
@@ -2,8 +2,8 @@ package model
 
 type Status struct {
 	ID          uint   `gorm:"primaryKey" json:"id"`
-	Name        string `gorm:"size:25;unique" json:"name"`
-	ValueSystem string `gorm:"size:200" json:"value_system"`
-	ValueUser   string `gorm:"size:250" json:"value_user"`
-	ValueNotif  string `gorm:"size:250" json:"value_notif"`
+	Name        string `gorm:"size:50;unique" json:"name"`
+	ValueSystem string `gorm:"size:300" json:"value_system"`
+	ValueUser   string `gorm:"size:300" json:"value_user"`
+	ValueNotif  string `gorm:"size:300" json:"value_notif"`
 }
