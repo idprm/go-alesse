dd163c512bd6393bc8a63a81353d72e13c858d05
Indra <indra@ide.web.id>
2022-12-29 17:50:58 +0700

    add new requestType in order

--- src/pkg/model/medicine.go
+++ src/pkg/model/medicine.go
@@ -4,6 +4,7 @@ type Medicine struct {
 	ID             uint64       `gorm:"primaryKey" json:"id"`
 	HealthcenterID uint         `json:"healthcenter_id"`
 	Healthcenter   Healthcenter `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+	Category       string       `gorm:"size:20" json:"category"`
 	Code           string       `gorm:"size:20" json:"code"`
 	Type           string       `gorm:"size:25" json:"type"`
 	Merk           string       `gorm:"size:200" json:"merk"`
