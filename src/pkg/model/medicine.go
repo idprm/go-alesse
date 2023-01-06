a9f532f1bcb4cab181e325fb98c020e83171bb74
Indra <indra@ide.web.id>
2023-01-06 11:10:50 +0700

    add new seeder medicines

--- src/pkg/model/medicine.go
+++ src/pkg/model/medicine.go
@@ -1,14 +1,10 @@
 package model
 
 type Medicine struct {
-	ID             uint64       `gorm:"primaryKey" json:"id"`
-	HealthcenterID uint         `json:"healthcenter_id"`
-	Healthcenter   Healthcenter `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
-	Category       string       `gorm:"size:20" json:"category"`
-	Code           string       `gorm:"size:20" json:"code"`
-	Type           string       `gorm:"size:25" json:"type"`
-	Merk           string       `gorm:"size:200" json:"merk"`
-	Name           string       `gorm:"size:200;not null" json:"name"`
-	Unit           string       `gorm:"size:25" json:"unit"`
-	IsActive       bool         `gorm:"type:bool;default:true" json:"is_active"`
+	ID       uint64 `gorm:"primaryKey" json:"id"`
+	Name     string `gorm:"size:200;not null" json:"name"`
+	Category string `gorm:"size:20" json:"category"`
+	Type     string `gorm:"size:25" json:"type"`
+	Unit     string `gorm:"size:25" json:"unit"`
+	IsActive bool   `gorm:"type:bool;default:true" json:"is_active"`
 }
